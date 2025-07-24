6fa7fd4e7a9b9d3517cfbba92e8022383a7bc61f
Darko-beep <darkofrederick98@outlook.com>
2025-07-24 18:01:46 +0000

    `Refactor components: AppHeader, ListItem, TodoList`

--- app/src/components/AppHeader.vue
+++ app/src/components/AppHeader.vue
@@ -1,30 +1,26 @@
-
-
 <template>
-    <header>
-        <h1><span>✔</span> To Do App</h1>
-        <p> Building Real World Web Application with Vue.js 3</p>
-    </header>
+  <header>
+    <h1><span></span> To Do App</h1>
+    <p>Building Real World Web Application with Vue.js 3</p>
+  </header>
 </template>
 
 <style scoped>
-
-header{
-    border-bottom:#333;
-    background-color: #fff;
+header {
+  border-bottom: 1px solid #ccc;
+  background-color: black;
 }
-header::after{
-    content: " ";
-    display:block;
-    height:1px;
-    box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);
+header::after {
+  content: ' ';
+  display: block;
+  height: 1px;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.75);
 }
-h1{
-    font-size: 2rem;
+h1 {
+  font-size: 2rem;
 }
-h1.icon{
-    font-size: 1rem;
-    vertical-align: midddle;
+h1.icon {
+  font-size: 1rem;
+  /* vertical-align: midddle; */
 }
-
 </style>
--- app/src/components/TodoList.vue
+++ app/src/components/TodoList.vue
@@ -1,46 +1,56 @@
 <script lang="ts" setup>
-import ListItem from './ListItem.vue';
+// Import the 'ref' function to create reactive state
+import { ref } from 'vue'
 
-type Item={
-    title:string,
-    checked?: boolean
+// Import the 'Ref' type for TypeScript typing of reactive variables
+import type { Ref } from 'vue'
+
+// Import the ListItem component (assumed to be a presentational component)
+import ListItem from './ListItem.vue'
+
+// Define a TypeScript type for individual items in the list
+type Item = {
+  title: string // Title or label of the task
+  checked?: boolean // Optional boolean indicating if task is completed
 }
-const ListItems: Item[]=[
-    {
-        title:"make an app",
-        checked:true
-    },
-    {
-        title:"go for a vacation",
-        checked:false
-    },
-    {
-        title:"attend lectures ",
-        checked:false
-    },
-    {
-        title:"Organize a program",
-        checked:false
-    }
-]
+
+// Define a reactive list of items using ref() and type annotation
+const listItems: Ref<Item[]> = ref([
+  { title: 'make an app', checked: true },
+  { title: 'go for a vacation', checked: false },
+  { title: 'attend lectures', checked: false },
+  { title: 'Organize a program', checked: false },
+])
+
+const updateItem = (item: Item): void => {
+  const updatedItem = findItemInList(item)
+  if (updatedItem) {
+    toggleItemChecked(updatedItem)
+  }
+}
+const findItemInList = (item: Item): Item | undefined => {
+  return listItems.value.find((itemInList: Item) => itemInList.title === item.title)
+}
+const toggleItemChecked = (item: Item): void => {
+  item.checked = !item.checked
+}
 </script>
 
 <template>
-    <ul>
-        <li :key="key" 
-        v-for="(item,key) in ListItems "
-        >
-        <ListItem :is-checked="item.checked">{{item.title}}</ListItem>
-
-        </li>
-    </ul>
+  <ul>
+    <li :key="key" v-for="(item, key) in listItems">
+      <ListItem :is-checked="item.checked" v-on:click.prevent="updateItem(item)">{{
+        item.title
+      }}</ListItem>
+    </li>
+  </ul>
 </template>
 
 <style scoped>
-ul{
-    list-style: none;
+ul {
+  list-style: none;
 }
-li{
-    margin: 0.4rem 0;
+li {
+  margin: 0.4rem 0;
 }
 </style>
